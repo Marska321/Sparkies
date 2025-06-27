08b111581c486f7ea3bb5dc38f5aa9b79aab30c5
Marska321 <mario.botha.mb@gmail.com>
2025-06-28 00:05:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext'; // Import the AuthProvider
+import { AuthProvider } from './contexts/AuthContext';
+import { ModalProvider } from './contexts/ModalContext'; // Import the ModalProvider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <ModalProvider> {/* Wrap App with ModalProvider */}
+        <App />
+      </ModalProvider>
     </AuthProvider>
   </React.StrictMode>
 );
